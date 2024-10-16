9ccd85646ec9eacc9ef40144b8d0083c803f5991
jayson <jayseip2004@gmail.com>
2024-10-15 21:29:48 -0400

    Finished to support mp3 and mp4 files

--- MyServer.java
+++ MyServer.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
 
 public class MyServer {
     final static int PORT = 8889;
@@ -26,9 +28,23 @@ public class MyServer {
                 if (fileRequested.equals("index.html")) {
                     File file = new File(fileRequested);
                     handleHTTPRequest(socket, file, true);
-                } else if (fileRequested.equals("logo.jpg")) {
+                }
+
+                // Checks for the logo.jpg
+                else if (fileRequested.equals("logo.jpg")) {
                     File file = new File(fileRequested);
                     handleHTTPRequest(socket, file, true);
+                }
+                // Checks for the audio.mp3 file
+                else if (fileRequested.equals("audio.mp3")) {
+                    File file = new File(fileRequested);
+                    handleHTTPRequest(socket, file, true);
+                }
+                // checks for the movie.mp4 file
+                else if (fileRequested.equals("movie.mp4")) {
+                    File file = new File(fileRequested);
+                    handleHTTPRequest(socket, file, true);
+
                 }
                 // Handles if the web page cannot be found.
                 else {
@@ -44,10 +60,11 @@ public class MyServer {
     }
 
     /**
+     * Handles HTTP request from the Client
      * 
-     * @param scoket connection
+     * @param socket connection
      * @param writer Writes the message to be sent to the client
-     * @param found  Determines if the web page is found or note
+     * @param found  Determines if the web page or file
      * @throws IOException
      * 
      */
@@ -55,12 +72,13 @@ public class MyServer {
             throws IOException {
 
         String fileName = file.getName();
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
         // If a File is found
         if (found) {
+            writer.println("HTTP/1.1 200 OK");
             if (fileName.endsWith(".html")) {
-                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
                 BufferedReader fileReader = new BufferedReader(new FileReader(file));
-                writer.println("HTTP/1.1 200 OK");
                 writer.println("Content-Type: text/html; charset=UTF-8");
 
                 // Reads the contents of the HTML file
@@ -69,17 +87,42 @@ public class MyServer {
                     writer.println(line);
                 }
                 fileReader.close();
-                writer.close();
             }
             if (fileName.endsWith(".jpg")) {
+                writer.println("Content-Type: image/jpeg");
+                writer.println();
+                BufferedImage image = ImageIO.read(file);
 
+                ImageIO.write(image, "jpg", socket.getOutputStream());
+                socket.getOutputStream().flush();
+            }
+            // Handles audio mp3 or video mp4 files since they are handled the same way
+            if (fileName.endsWith(".mp3") || fileName.endsWith(".mp4")) {
+                if (fileName.endsWith(".mp3")) {
+                    writer.println("Content-Type: audio/mpeg");
+                } else {
+                    writer.println("Content-Type: video/mp4");
+                    writer.println("Content-Length: " + file.length());
+                    System.out.println("Video");
+                    System.out.println(file.length() + " bytes.");
+                }
+                writer.println();
+
+                byte[] buffer = new byte[16384];
+                int bytesRead;
+                FileInputStream inputStream = new FileInputStream(file);
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    socket.getOutputStream().write(buffer, 0, bytesRead);
+                }
+                socket.getOutputStream().flush();
+
+                inputStream.close();
             }
 
         }
         // If webpage is not found process a 404 HTTP reply
         else {
             BufferedReader fileReader = new BufferedReader(new FileReader(file));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             writer.println("HTTP/1.1 404 Not Found");
             writer.println("Content-Type: text/html; charset=UTF-8");
             String line;
@@ -87,7 +130,8 @@ public class MyServer {
                 writer.println(line);
             }
             fileReader.close();
-            writer.close();
+
         }
+        writer.close();
     }
 }
